c858f1e8e3ec33078eeac2fdcecfe01913d12fa1
Maxime Thirouin <m@moox.io>
2014-03-07 06:46:04 +0100

    Update Node & NPM installation to avoid homebrew npm update -g error
    Thanks to @kud tip
    https://github.com/npm/npm/issues/3794#issuecomment-36921580

--- components/node/aliases.zsh
+++ components/node/aliases.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
-export NODE_MODULES=/usr/local/lib/node_modules
 
-# Faster npm for europeans
+# npme: faster npm for europeans
 command -v npm > /dev/null && alias npme="npm --registry http://registry.npmjs.eu"
+
+# global npm update
 alias npmug="npm update -g --loglevel warn"
---
+++ components/node/path.zsh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+export NODE_PATH=/usr/local/lib/node_modules

f61f45d9d880df88dfa6dcef7c14d1e4c80923da
Maxime Thirouin <m@moox.io>
2014-05-05 07:16:48 +0200

    Adjust apache aliases to use $EDITOR not subl

--- components/amp/aliases.zsh
+++ components/amp/aliases.zsh
@@ -2,12 +2,12 @@
 
 export APACHE_CONF="/etc/apache2/httpd.conf"
 export APACHE_CONF_DEV_PATH="$DIR_DEV/_.conf/apache/"
-alias apacheconf="subl $APACHE_CONF"
+alias apacheconf="e $APACHE_CONF"
 alias apacheconfigtest="apachectl configtest"
 alias apachestart="sudo apachectl start"
 alias apacherestart="flushdns && sudo apachectl restart"
 
-alias apacheaddconf="subl $APACHE_CONF_DEV_PATH/local.conf /etc/hosts"
+alias apacheaddconf="e $APACHE_CONF_DEV_PATH/local.conf /etc/hosts"
 
 # nginx
 # alias nginx="sudo nginx"
@@ -17,7 +17,7 @@ alias apacheaddconf="subl $APACHE_CONF_DEV_PATH/local.conf /etc/hosts"
 # php
 export PHP_PATH="$(brew --prefix josegonzalez/php/php55)"
 export PATH="$PHP_PATH/bin:$PATH"
-alias phpini="subl /usr/local/etc/php/5.5/php.ini"
+alias phpini="e /usr/local/etc/php/5.5/php.ini"
 
 # MySQL (from brew)
 alias mysqlstart="mysql.server start"

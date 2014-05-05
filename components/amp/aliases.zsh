07faf893236cad7991bd8946b2577bf4972a7df0
Maxime Thirouin <m@moox.io>
2014-05-05 07:16:57 +0200

    Add a apachedevconf alias

--- components/amp/aliases.zsh
+++ components/amp/aliases.zsh
@@ -3,6 +3,7 @@
 export APACHE_CONF="/etc/apache2/httpd.conf"
 export APACHE_CONF_DEV_PATH="$DIR_DEV/_.conf/apache/"
 alias apacheconf="e $APACHE_CONF"
+alias apachedevconf="e $APACHE_CONF_DEV_PATH"
 alias apacheconfigtest="apachectl configtest"
 alias apachestart="sudo apachectl start"
 alias apacherestart="flushdns && sudo apachectl restart"

c1e0ad27152febc1cf9201a6a7c23faba16d8109
Maxime Thirouin <m@moox.io>
2014-06-12 07:48:28 +0200

    Add Synergy.app conf+aliases

---
+++ components/lazydev/aliases.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+alias synergys="$HOME/Applications/Synergy.app/Contents/MacOS/synergys"
+alias synergyc="$HOME/Applications/Synergy.app/Contents/MacOS/synergyc"
+
+alias synergyshome="synergys -n iMoOx"
+alias synergychome="synergys -n MacMoOx iMoOx.local"

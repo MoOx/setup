4b7ab39e1ea1fce3e96f9390ecc4a938506c3cd1
Maxime Thirouin <m@moox.io>
2014-06-23 08:06:15 +0200

    Fix synergy client alias

--- components/lazydev/aliases.zsh
+++ components/lazydev/aliases.zsh
@@ -4,4 +4,4 @@ alias synergys="$HOME/Applications/Synergy.app/Contents/MacOS/synergys"
 alias synergyc="$HOME/Applications/Synergy.app/Contents/MacOS/synergyc"
 
 alias synergyshome="synergys -n iMoOx"
-alias synergychome="synergys -n MacMoOx iMoOx.local"
+alias synergychome="synergyc -n MacMoOx iMoOx.local"

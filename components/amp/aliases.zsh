bede3a2945216319158c1870d92713a0d825f55f
Maxime Thirouin <m@moox.io>
2014-07-22 07:01:23 +0200

    Remove apache conf useless trailing slash

--- components/amp/aliases.zsh
+++ components/amp/aliases.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 export APACHE_CONF="/etc/apache2/httpd.conf"
-export APACHE_CONF_DEV_PATH="$DIR_DEV/_.conf/apache/"
+export APACHE_CONF_DEV_PATH="$DIR_DEV/_.conf/apache"
 alias apacheconf="e $APACHE_CONF"
 alias apachedevconf="e $APACHE_CONF_DEV_PATH"
 alias apacheconfigtest="apachectl configtest"

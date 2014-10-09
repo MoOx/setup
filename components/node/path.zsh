70080d6cdc081a6a197debcc909318b912726301
Maxime Thirouin <m@moox.io>
2014-10-09 07:00:49 +0200

    explode PATH update in appropriate place

--- components/node/path.zsh
+++ components/node/path.zsh
@@ -2,5 +2,4 @@
 
 export NODE_PATH=/usr/local/lib/node_modules
 
-# local bin, then local npm bin, then global npm bin
-export PATH=./bin:./node_modules/.bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$PATH
+# export PATH=./node_modules/.bin:$PATH

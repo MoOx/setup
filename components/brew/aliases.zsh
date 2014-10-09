0874ffbc3e4c9240c87a29547dd1ffd6f96610cd
Maxime Thirouin <m@moox.io>
2014-10-09 07:12:46 +0200

    Better brew update alias

--- components/brew/aliases.zsh
+++ components/brew/aliases.zsh
@@ -1,2 +1,2 @@
 command -v brew > /dev/null && alias caskroom="cd $(brew --prefix)/Library/Taps/caskroom"
-alias brewu="brew update && brew upgrade"
+alias brewu="brew update && brew upgrade && brew cleanup && brew cask cleanup"

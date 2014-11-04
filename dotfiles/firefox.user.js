43412d8a530811a81009025b0ae4a116c6b38b8b
Maxime Thirouin <m@moox.io>
2014-11-04 20:21:16 +0100

    firefox config
    close #16

---
+++ dotfiles/firefox.user.js
@@ -0,0 +1,7 @@
+// var userPref = this.user_pref
+
+user_pref("general.warnOnAboutConfig", false);
+
+user_pref("browser.newtab.url", "http://putaindecode.io/tab/");
+
+user_pref("mousewheel.with_meta.action", "0"); // disable zoom on cmd + mousewhell

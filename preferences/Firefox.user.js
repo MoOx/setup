38520b7677640f624e65b77a2837253923cb9cba
Maxime Thirouin <m@moox.io>
2014-11-19 08:10:15 +0100

    fix firefox disabled zoom on mousewheel

--- preferences/Firefox.user.js
+++ preferences/Firefox.user.js
@@ -4,4 +4,4 @@ user_pref("general.warnOnAboutConfig", false);
 
 user_pref("browser.newtab.url", "http://putaindecode.io/tab/");
 
-user_pref("mousewheel.with_meta.action", "0"); // disable zoom on cmd + mousewhell
+user_pref("mousewheel.with_meta.action", 0); // disable zoom on cmd + mousewheel

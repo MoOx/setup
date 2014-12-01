36e349762b3e0c21410ae849e054a213a85191ab
Maxime Thirouin <m@moox.io>
2014-12-01 19:38:15 +0100

    Back to Firefox nighlty

--- preferences/Firefox.user.js
+++ preferences/Firefox.user.js
@@ -1,7 +1,11 @@
-// var userPref = this.user_pref
+/* globals user_pref */
 
 user_pref("general.warnOnAboutConfig", false);
 
 user_pref("browser.newtab.url", "http://putaindecode.io/tab/");
 
 user_pref("mousewheel.with_meta.action", 0); // disable zoom on cmd + mousewheel
+
+user_pref("browser.devedition.theme.enabled", true); // Firefox Dev Edition skin
+
+user_pref("devtools.theme", "dark");

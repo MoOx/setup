94da60921883bd69ebf9b4686799ca9f0ed34ffc
Maxime Thirouin <m@moox.io>
2014-12-09 16:15:48 +0100

    Update firefox preferences

--- preferences/Firefox.user.js
+++ preferences/Firefox.user.js
@@ -1,11 +1,21 @@
 /* globals user_pref */
 
-user_pref("general.warnOnAboutConfig", false);
-
 user_pref("browser.newtab.url", "http://putaindecode.io/tab/");
 
-user_pref("mousewheel.with_meta.action", 0); // disable zoom on cmd + mousewheel
+// remove warning when accessing about:config
+user_pref("general.warnOnAboutConfig", false);
 
-user_pref("browser.devedition.theme.enabled", true); // Firefox Dev Edition skin
+// disable zoom on cmd + mousewheel
+user_pref("mousewheel.with_meta.action", 0);
 
-user_pref("devtools.theme", "dark");
+// Firefox Dev Edition skin
+user_pref("browser.devedition.theme.enabled", true);
+
+// light skin
+user_pref("devtools.theme", "light");
+
+// devtools for the chrome & add-ons
+user_pref("devtools.chrome.enabled", true);
+
+// remote debugging
+user_pref("devtools.debugger.remote-enabled", true);

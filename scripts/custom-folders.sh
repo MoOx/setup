602ef4b1a83d4e16e9e74a44f83632c9b8045636
Maxime Thirouin <git@moox.io>
2019-10-09 21:42:23 +0200

    Update custom-folders.sh

--- scripts/custom-folders.sh
+++ scripts/custom-folders.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+chflags -h nohidden $HOME/Library
+
+
 # ~/Sync/ folder + icon
 if ! ls $DIR_SYNC &> /dev/null
 then
@@ -18,40 +21,3 @@ lnfs-subfolders $DIR_SYNC/Library $HOME/Library
 
 # Sync/Library/Application Support/*
 lnfs-subfolders "$DIR_SYNC/Library_Application Support" "$HOME/Library/Application Support"
-
-# Sync/Library/Containers/*
-# (eg: iBooks)
-lnfs-subfolders "$DIR_SYNC/Library_Containers" "$HOME/Library/Containers"
-
-# Sync/iTunes
-if ls $DIR_SYNC/iTunes &> /dev/null
-then
-  rm -rf $HOME/Music/iTunes &> /dev/null
-  ln -s $DIR_SYNC/iTunes $HOME/Music/iTunes
-fi
-
-# Sync/Pictures
-if ls $DIR_SYNC/Pictures &> /dev/null
-then
-  sudo rm -rf $HOME/Pictures
-  ln -s $DIR_SYNC/Pictures $HOME/Pictures
-fi
-
-# Shared Photos
-SHARED_PHOTOS="/Users/Shared/Photos"
-if ! ls $SHARED_PHOTOS &> /dev/null
-then
-  if ls $DIR_SYNC/Photos &> /dev/null
-  then
-    ln -s $DIR_SYNC/Photos $SHARED_PHOTOS
-  elif ls "/Volumes/Shared HD/Photos" &> /dev/null
-  then
-    ln -s "/Volumes/Shared HD/Photos" $SHARED_PHOTOS
-  fi
-fi
-
-# hide macOS unused folders
-chflags -h hidden $HOME/Documents
-chflags -h hidden $HOME/Movies
-chflags -h hidden $HOME/Music
-chflags -h hidden $HOME/Pictures
